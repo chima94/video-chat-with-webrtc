8b7338ab26eef23c2549a1018621b4ecd7cabc5f
chima94 <chimanwakis@gmail.com>
2023-01-26 13:10:45 +0100

    done

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const console = require("console");
 const express = require("express");
 const http = require("http");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 const app = express();
 const server = http.createServer(app);
